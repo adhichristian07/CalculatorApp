2ddce5436e9e1b5ff2fc06838159ef54238daa78
Adhi Christian <43411668+adhichristian07@users.noreply.github.com>
2018-09-27 15:20:11 +0700

    Add files via upload

---
+++ CalculatorAppV1/CalculatorAppV1/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace CalculatorAppV1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.NilaiA = new System.Windows.Forms.TextBox();
+            this.NilaiB = new System.Windows.Forms.TextBox();
+            this.Hasil = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(76, 153);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Operasi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(76, 223);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nilai A";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(76, 292);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nilai B ";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Penambahan ( + )",
+            "Pengurangan ( - )",
+            "Perkalian ( x )",
+            "Pembagian ( : )"});
+            this.comboBox1.Location = new System.Drawing.Point(178, 148);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(151, 32);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.Text = "Pilih";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(76, 363);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 24);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Hasil";
+            // 
+            // NilaiA
+            // 
+            this.NilaiA.Location = new System.Drawing.Point(178, 219);
+            this.NilaiA.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.NilaiA.Name = "NilaiA";
+            this.NilaiA.Size = new System.Drawing.Size(151, 28);
+            this.NilaiA.TabIndex = 5;
+            // 
+            // NilaiB
+            // 
+            this.NilaiB.Location = new System.Drawing.Point(178, 287);
+            this.NilaiB.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.NilaiB.Name = "NilaiB";
+            this.NilaiB.Size = new System.Drawing.Size(151, 28);
+            this.NilaiB.TabIndex = 6;
+            // 
+            // Hasil
+            // 
+            this.Hasil.Location = new System.Drawing.Point(178, 357);
+            this.Hasil.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Hasil.Name = "Hasil";
+            this.Hasil.Size = new System.Drawing.Size(151, 28);
+            this.Hasil.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(203, 439);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 35);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Hajar ! ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label5.Font = new System.Drawing.Font("Hobo Std", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label5.Location = new System.Drawing.Point(12, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(261, 34);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Calculator V.1 (Beta)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 43);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(165, 24);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "(One Piece Theme)";
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 543);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(215, 44);
+            this.axWindowsMediaPlayer1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
+            this.BackgroundImage = global::CalculatorAppV1.Properties.Resources.Gambar_Wallpaper_Monkey_de_Luffy_One_Piece_Terlengkap41;
+            this.ClientSize = new System.Drawing.Size(741, 529);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Hasil);
+            this.Controls.Add(this.NilaiB);
+            this.Controls.Add(this.NilaiA);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Hobo Std", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Calculator V.1 (Beta)";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox NilaiA;
+        private System.Windows.Forms.TextBox NilaiB;
+        private System.Windows.Forms.TextBox Hasil;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+    }
+}
+
---
+++ CalculatorAppV1/CalculatorAppV1/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace CalculatorAppV1
+{
+    public partial class Form1 : Form
+    {
+        WindowsMediaPlayer player = new WindowsMediaPlayer();
+        public Form1()
+        {
+            InitializeComponent();
+            player.URL = "James Arthur - Say You Won't Let Go.mp3";
+        }
+
+      
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            player.controls.play();
+
+            int a = int.Parse(NilaiA.Text); //proses casting
+            var b = Convert.ToInt32(NilaiB.Text); //casting
+            Double lstHasil;
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    lstHasil = a + b;
+                    Hasil.Text = Convert.ToString(lstHasil);
+                    break;
+                case 1:
+                    lstHasil = a - b;
+                    Hasil.Text = Convert.ToString(lstHasil);
+                    break;
+                case 2:
+                    lstHasil = a * b;
+                    Hasil.Text = Convert.ToString(lstHasil);
+                    break;
+                case 3:
+                    lstHasil = a / b;
+                    Hasil.Text = Convert.ToString(lstHasil);
+                    break;
+            }
+        }
+    }
+}
